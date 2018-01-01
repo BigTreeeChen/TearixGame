f5ba7ac425c1ed5d74de388e0a9a982870dd562a
Tearix <chshyue@foxmail.com>
2018-01-01 23:28:16 +0800

    first diff commit

--- Scripts/UI/MainView/SkillButton.cs
+++ Scripts/UI/MainView/SkillButton.cs
@@ -1,15 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
+using SGameSetting;
 
-public class SkillButton : MonoBehaviour {
+namespace TGame.UI
+{
+    public class SkillButton : MonoBehaviour
+    {
+        public static SkillButton[] list = new SkillButton[5];
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        [SerializeField]
+        GameObject mIcon;
+        [SerializeField]
+        UITexture mIconTexture;
+        [SerializeField]
+        Transform mChaseCircleTrans;
+        [SerializeField]
+        int mSlot;
+
+        private UITweener[] mPressTweens;
+        private int mUseSkillID;
+        private int mPressMode;
+        private bool mIsPress;
+        private bool mIsStarted = false;
+
+        void Start()
+        {
+            list[mSlot] = this;
+            mPressTweens = transform.Find("FrameCenter").GetComponentsInChildren<UITweener>();
+            mIsStarted = true;
+        }
+
+        void ReloadSkill()
+        {
+            mIcon.SetActive(true);
+            // set icontexture
+        }
+
+        public static int GetPressMode(int skillID)
+        {
+            if (skill.data.ContainsKey(skillID))
+            {
+                return skill.data[skillID].press_mode;
+            }
+            return 2;
+        }
+
+        public static int GetUseSkillID(int _slot)
+        {
+            int job = 0;
+            return _slot;
+        }
+    }
+
 }
